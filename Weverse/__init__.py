29093903b58f8cec7903604bd55d851a10bc18c0
Mujy <thugystory1@gmail.com>
2021-05-20 17:13:14 -0400

    Revert

--- Weverse/__init__.py
+++ Weverse/__init__.py
@@ -3,7 +3,6 @@ from . import models
 from .error import InvalidToken, PageNotFound, BeingRateLimited
 from .objects import create_tab_objects, create_community_objects, create_comment_objects, create_notification_objects,\
     create_media_object, create_post_objects, create_artist_objects, create_photo_objects
+from .weverseclient import WeverseClient
 from .weversesync import WeverseClientSync
 from .weverseasync import WeverseClientAsync
-from .weverseclient import WeverseClient
-
