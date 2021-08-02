8bd63af53b9be0ecc253863e650e24e49fb18c86
Mujy <thugystory1@gmail.com>
2021-08-02 17:34:24 -0400

    removed outdated package

--- Weverse/__init__.py
+++ Weverse/__init__.py
@@ -10,7 +10,7 @@ from .objects import create_tab_objects, create_community_objects, create_commen
 __title__ = 'Weverse'
 __author__ = 'MujyKun'
 __license__ = 'MIT'
-__version__ = '1.0.7.2'
+__version__ = '1.0.7.3'
 
 
 def check_expired_token(func):
